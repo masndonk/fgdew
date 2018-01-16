0eb897210e1e422b0f70322a1ff63e6e47b7c561
masndonk <35361954+masndonk@users.noreply.github.com>
2018-01-15 19:51:58 -0800

    Create tes.js

---
+++ tes.js
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LiveFlings</title>
+    
+        <meta name="description" content="The best sex personals. Browse and meet local singles right from your phone." />
+<meta name="keywords" content="mobile dating sites, mobile dating, mobile local dating, mobile personals, mobile online dating" />
+        <link rel="stylesheet" type="text/css" href="https://mobile.whitelabelpros.com/css/styles_1107_2.css" />
+    <link rel="stylesheet" type="text/css" href="https://mobile.whitelabelpros.com/css/styles_1107_2_pages.css" />
+    
+    <script src="https://mobile.whitelabelpros.com/js/main.js"></script>
+    <script src="https://mobile.whitelabelpros.com/js/formLibrary.js"></script>
+                            <script type="text/javascript">
+            $($.mobile.document).on("pagecontainershow", function() {
+                setTimeout(function() {
+                    $(".signupForm").trigger("greCaptchaLoaded");
+                }, 2000);
+            });
+            function onSubmitRecaptcha(token) {
+                var activePage = $.mobile.pageContainer.pagecontainer("getActivePage"),
+                    signupForm = activePage.find('.signupForm');
+                signupForm.trigger('greCaptchaSubmitted', token);
+            }
+        </script>
+        <script src="https://www.google.com/recaptcha/api.js?render=explicit" async defer></script>
+                <script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
+    </head>
+
+<body class="outside">
+            <div class="pushsubscription" id="pushsubscription" data-role="pushsubscription"></div>
+                
+        
+        <div class="landingPage_73 landingCont" data-role="page" data-url="/?skin=73">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:500,600,700" rel="stylesheet">
+    <div data-role="header" class="landingHeader">
+        <a href="/login" class="login" data-transition="slide" data-direction="reverse" data-prefetch>Login</a>
+<a href="/signup" class="register" data-transition="slide" data-prefetch>Sign Up</a>
+<a href="https://mobile.liveflings.com/?ref=logo" class="logo" notranslate>LiveFlings Mobile</a>
+    </div>
+    <div class="main">
+        <h2>
+            <strong>Warning!</strong>
+            You Will See Nude Photos
+        </h2>
+        <div class="imgContainer">
+            <span class="img"></span>
+        </div>
+        <h3>Are you a male or female?</h3>
+        <div class="genderButtons">
+            <a href="/signup?iam=men" class="genderButton">
+                <span class="circle male">
+                    <i class="fa fa-mars" aria-hidden="true"></i>
+                </span>
+                <span class="label">Male Looking For Female</span>
+            </a>
+            <a href="/signup?iam=women" class="genderButton">
+                <span class="circle female">
+                    <i class="fa fa-venus" aria-hidden="true"></i>
+                </span>
+                <span class="label">Female Looking For Male</span>
+            </a>
+        </div>
+    </div>
+    <div class="stepIndicators">
+        <ul>
+            <li class="step1 active">
+                <span class="stepIndicator"></span>
+            </li>
+            <li class="step2">
+                <span class="stepIndicator"></span>
+            </li>
+            <li class="step3">
+                <span class="stepIndicator"></span>
+            </li>
+            <li class="step4">
+                <span class="stepIndicator"></span>
+            </li>
+            <li class="step5">
+                <span class="stepIndicator"></span>
+            </li>
+            <li class="step6">
+                <span class="stepIndicator"></span>
+            </li>
+        </ul>
+    </div>
+    <div data-role="footer" class="landingFooter">
+        <div class="footerNav cf">
+    <a href="https://mobile.liveflings.com/page/faq">FAQ</a>
+    <a href="https://mobile.liveflings.com/page/contact">Contact</a>
+    <a href="https://mobile.liveflings.com/page/privacy">Privacy</a>
+    <a href="https://mobile.liveflings.com/page/terms">Terms</a>
+    <a href="https://mobile.liveflings.com/page/antispam">Anti-Spam</a>
+    <a href="https://mobile.liveflings.com/page/dmca">DMCA</a>
+</div>
+<p class="footerCopyright">Copyright &copy;1999-2018 <span notranslate>LiveFlings</span> All rights reserved.</p>
+    <p><a class="compl_link" href="https://mobile.liveflings.com/page/compliance">18 U.S.C. 2257 Record-Keeping Requirements<br/>Compliance Statement</a></p>
+<p class="footerFull"><a href="/fullsite" rel="external">Desktop Version</a><br /></p>    </div>
+</div>
+            
+    
+    
+</body>
+</html>
